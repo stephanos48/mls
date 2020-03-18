003f7d78e1ec84ed4d22aadb2b84be8668cf73ba
Stephen English <stephanos.english@gmail.com>
2020-03-17 21:59:30 -0500

    emailupdate20200317

--- mls/mls/Controllers/HomeController.cs
+++ mls/mls/Controllers/HomeController.cs
@@ -7,6 +7,8 @@ using mls.Models;
 using mls.ViewModels;
 using System.Data.Entity;
 using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
 
 namespace mls.Controllers
 {
@@ -39,6 +41,38 @@ namespace mls.Controllers
             //{
             //    return RedirectToAction("Index", "Home");
             //}
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(EmailFormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("stephen.english@unitedhyd.com"));  // replace with valid value 
+                message.From = new MailAddress("stephanos.englisch@hotmail.com");  // replace with valid value
+                message.Subject = "Your email subject";
+                message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
+                message.IsBodyHtml = true;
+
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                        UserName = "stephanos.englisch@hotmail.com",  // replace with valid value
+                        Password = "Rea1madrid"  // replace with valid value
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = "smtp-mail.outlook.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    await smtp.SendMailAsync(message);
+                    return RedirectToAction("Sent");
+                }
+            }
+            return View(model);
         }
 
         public ActionResult About()
